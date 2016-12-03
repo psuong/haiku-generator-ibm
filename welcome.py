fbe17b07927d7b2da929fcdc73e79d419063f501
Porrith Suong <psuong95@gmail.com>
2016-12-03 17:15:14 -0500

    Adds materialize to the repo.

--- welcome.py
+++ welcome.py
@@ -17,28 +17,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def Welcome():
+def index():
     return "Hello world"
 
-@app.route('/myapp')
-def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
-
-@app.route('/api/people')
-def GetPeople():
-    list = [
-        {'name': 'John', 'age': 28},
-        {'name': 'Bill', 'val': 26}
-    ]
-    return jsonify(results=list)
-
-@app.route('/api/people/<name>')
-def SayHello(name):
-    message = {
-        'message': 'Hello ' + name
-    }
-    return jsonify(results=message)
 
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
