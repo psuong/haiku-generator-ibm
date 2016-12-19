5303a8c1a1b7dea8a0ecda88b87ceb327e9380a1
Porrith Suong <psuong95@gmail.com>
2016-12-19 18:30:27 -0500

    Adds whitespace removal, TODOs, function clearing.

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
+#Set whitespace trimming
+app.jinja_env.trim_blocks = True;
+app.jinja_env.lstrip_blocks = True;
+
 # Global Variables
 post_request = "POST"
 get_request = "GET"
@@ -27,6 +31,8 @@ def index():
     """
     Renders the default page for the app to render.
     """
+
+    # General dictionary to hold all of the variables in the html template
     context_dict = {
         "page_title": "Haiku Generator",
         "form_action": "/",
@@ -34,10 +40,19 @@ def index():
     }
 
     if request.method == post_request:
-        return "POST"
+        # Update the haiku if the app received a post request
+        # TODO: Write a function which generates the sentences
+        haiku = []
+        haiku.append("An old pond")
+        haiku.append("A frog jumps in --")
+        haiku.append("the sound of water.")
+        context_dict["haiku"] = haiku
+
+    # Render the template here
     return render_template("main.html", **context_dict)
 
 
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
+    # TODO: Make sure to change the host from local to external
 	app.run(debug=True, host='127.0.0.1', port=int(port))
