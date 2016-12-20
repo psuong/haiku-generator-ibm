919ef2048f7ed4cac8720c586aa95629a190e0da
Porrith Suong <psuong95@gmail.com>
2016-12-19 19:03:42 -0500

    Adds the haiku_generator file, fill in the functions.

---
+++ haiku_generator.py
@@ -0,0 +1,8 @@
+def generate_line(syllable_count):
+    # TODO: Generate the logic which defines the line in a haiku
+    pass
+
+def generate_random_line(syllable_count):
+    # TODO: Generate the line by randomly selecting words, this should be relatively
+    # easy to do
+    pass
